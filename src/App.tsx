47023cf9dfcfeb706b78b0430052dde550eedb00
Vishal Chand <sam.chand96@gmail.com>
2024-12-02 18:04:33 +0530

    Wrote the game logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
+import Icon from './components/Icon';
+import Snackbar from 'react-native-snackbar';
+
+// constants
+const empty = 'empty'
+const circle = 'cicle'
+const cross = 'cross'
+const defIcon = 'pencil'
+
 import {
   SafeAreaView,
   ScrollView,
@@ -9,62 +18,116 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-// function Section({children, title}: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
+import {getVersion} from 'jest';
 
 function App(): React.JSX.Element {
+  const [isCross, setIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setGameWinner('');
+    setGameState(new Array(9).fill('empty', 0, 9));
+  };
+
+  const checkGameWinner = () => {
+    //row check
+    if (
+      gameState[0] != empty &&
+      gameState[0] == gameState[1] &&
+      gameState[1] == gameState[2]
+    ) {
+      setGameWinner(`${gameState[0]} has won the game 👿️!!`);
+    } else if (
+      gameState[3] != empty &&
+      gameState[3] == gameState[4] &&
+      gameState[4] == gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} has won the game 👿️!!`);
+    } else if (
+      gameState[6] != empty &&
+      gameState[6] == gameState[7] &&
+      gameState[7] == gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} has won the game 👿️!!`);
+    } else if (
+      //column check
+      gameState[0] != empty &&
+      gameState[0] == gameState[3] &&
+      gameState[3] == gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} has won the game 👿️!!`);
+    } else if (
+      gameState[1] != empty &&
+      gameState[1] == gameState[4] &&
+      gameState[4] == gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} has won the game 👿️!!`);
+    } else if (
+      gameState[2] != empty &&
+      gameState[2] == gameState[5] &&
+      gameState[5] == gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} has won the game 👿️!!`);
+    } else if (
+      //checking dialonally
+      gameState[0] != empty &&
+      gameState[0] == gameState[4] &&
+      gameState[4] == gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} has won the game 👿️!!`);
+    } else if (
+      gameState[2] != empty &&
+      gameState[2] == gameState[4] &&
+      gameState[4] == gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} has won the game 👿️!!`)
+    } else if(
+      //draw case
+      !gameState.includes(empty, 0)
+    ) {
+        setGameWinner(`Game draw....⏳️`)
+    }
+  };
+
+  const onItemChanged = (index: number) =>  {
+    if(gameWinner) {
+      return Snackbar.show({
+        text: gameWinner, 
+        textColor: '#', 
+        backgroundColor: '#'
+      })
+    }
+    if(gameState[index] === empty) {
+      gameState[index] = isCross ? cross : circle
+      setIsCross(!isCross)
+    } else {
+      return Snackbar.show({
+        text: 'No allowed', 
+        textColor: '#ffffff', 
+        backgroundColor: '#F04A00'
+      })
+    }
+    checkGameWinner()
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
-        <Text style={[styles.textWhite, styles.headingTxt]}>Tic-Tac-Toe</Text>
+      <Text style={[styles.textWhite, styles.headingTxt]}>Tic-Tac-Toe</Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   textWhite: {
-    padding: 8, 
-    color: '#ffffff', 
-    fontWeight: 'bold'
-  }, 
+    padding: 8,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
   headingTxt: {
-    fontSize: 24
+    fontSize: 24,
   },
   sectionContainer: {
     marginTop: 32,
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 import type {PropsWithChildren} from 'react';
 import Icons from 'react-native-vector-icons/FontAwesome';
 
-
 //defining prop for Icons
 type IconProps = PropsWithChildren<{
   name: string;
@@ -12,25 +11,11 @@ type IconProps = PropsWithChildren<{
 const Icon = ({name}: IconProps) => {
   switch (name) {
     case 'cross':
-        return <Icons 
-        name='circle-thin'
-        color='#'
-        size={34} />
-        break;
-    case 'circle': 
-        <Icons 
-        name='times'
-        color='#'
-        size={34}
-        />
-        break; 
+      return <Icons name="times" color="#FE0000" size={34} />;
+    case 'circle':
+      return <Icons name="circle-thin" color="#00FF40" size={34} />;
     default:
-        <Icons 
-        name='pencil'
-        color='#'
-        size={34}
-        />
-        break;
+      return <Icons name="pencil" color="#ffffff" size={34} />;
   }
 };
 
